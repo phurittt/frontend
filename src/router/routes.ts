1babc0cd910c913ec3b3c51f0da96ee4f756c587
Teeranut Nokhong <66160067@go.buu.ac.th>
2026-03-10 15:50:24 +0700

    optimize route structure

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,9 @@
 import type { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
+  // ==========================================
+  // กลุ่มของผู้ใช้งานทั่วไป (ใช้ MainLayout)
+  // ==========================================
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
@@ -9,31 +12,28 @@ const routes: RouteRecordRaw[] = [
       { path: 'courses/all', component: () => import('pages/CoursePage.vue') },
       { path: 'courses/open', component: () => import('pages/CoursePage.vue') },
       { path: 'courses/:id', component: () => import('pages/CourseDetailPage.vue') },
+
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+      { path: 'register', component: () => import('pages/RegisterPage.vue') },
     ],
   },
-  {
-    path: '/login',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/LoginPage.vue') }],
-  },
-  {
-    path: '/register',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/RegisterPage.vue') }],
-  },
+
+  // ==========================================
+  // กลุ่มของระบบหลังบ้าน (ใช้ AdminLayout)
+  // ==========================================
   {
     path: '/admin',
     component: () => import('layouts/AdminLayout.vue'),
     children: [
       { path: '', redirect: '/admin/projects' },
       { path: 'projects', component: () => import('pages/admin/ProjectManagePage.vue') },
-
-      {
-        path: 'project-types',
-        component: () => import('pages/admin/CourseProjectTypes.vue')
-      },
+      { path: 'project-types', component: () => import('pages/admin/CourseProjectTypes.vue') },
     ],
   },
+
+  // ==========================================
+  // หน้า 404
+  // ==========================================
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
