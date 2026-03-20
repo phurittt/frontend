c6c25d94bdcfa262324e15c207bfdc67b543db44
Thanapat Sirisopolporn <66160064@go.buu.ac.th>
2026-03-21 04:01:55 +0700

    framework add plugins 'Dialog'(ไม่มีได้ไงเนี่ย)

--- quasar.config.ts
+++ quasar.config.ts
@@ -117,7 +117,7 @@ export default defineConfig((ctx) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Notify'],
+      plugins: ['Dialog', 'Notify'],
     },
 
     // animations: 'all', // --- includes all animations
